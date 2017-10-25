ac19f582b0fe81b89f0dcb97cb536c578e86be1f
mark <markwani70@googlemail.com>
2017-10-25 16:26:50 +0100

    Edited settings for amazon s3

--- settings/base.py
+++ settings/base.py
@@ -98,7 +98,7 @@ USE_TZ = True
 DISQUS_WEBSITE_SHORTNAME = 'QAblog'
 SITE_ID = 1
 
-STATIC_URL = 'https://thqablogmain.eu-west-2-056499317226.s3.amazonaws.com/'
+STATIC_URL = 'https://thqablogmain.s3.eu-west-2.amazonaws.com/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
@@ -115,7 +115,7 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=94608000',
 }
 
-AWS_STORAGE_BUCKET_NAME = 'https://thqablogmain.s3.eu-west-2.amazonaws.com/'
+AWS_STORAGE_BUCKET_NAME = 'thqablogmain.s3.eu-west-2'
 AWS_S3_REGION_NAME = 'eu-west-2'  # e.g. us-east-2
 AWS_ACCESS_KEY_ID = 'AKIAIQ3ITTBLKAKFZRTQ'
 AWS_SECRET_ACCESS_KEY = 'Q14yiGN8mpjgMR0xi9bYc9Wd/RorhJyDUOM9XABg'
@@ -127,8 +127,3 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 # you run `collectstatic`).
 STATIC_FILES STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATICFILES_LOCATION = 'static'
-STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-
-MEDIAFILES_LOCATION = 'media'
-DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
