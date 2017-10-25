95b86af7532a0a70e7334ae76a4e0dbebd9afaa1
mark <markwani70@googlemail.com>
2017-10-25 14:01:32 +0100

    Edited settings.py file

--- settings/base.py
+++ settings/base.py
@@ -95,19 +95,19 @@ USE_L10N = True
 
 USE_TZ = True
 
+DISQUS_WEBSITE_SHORTNAME = 'QAblog'
+SITE_ID = 1
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 AWS_S3_OBJECT_PARAMETERS = {
     'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
     'CacheControl': 'max-age=94608000',
 }
 
-AWS_STORAGE_BUCKET_NAME = 'theqablog'
-AWS_S3_REGION_NAME = 'eu-west-2'  # e.g. us-east-2
-AWS_ACCESS_KEY_ID = 'AKIAJUXJU6NWMVFHNFPA'
-AWS_SECRET_ACCESS_KEY = '1Hx86X+rvpH+fhaxy83cZRnrq3mPy3HDp491Ghij'
+AWS_STORAGE_BUCKET_NAME = 'thqablogmain'
+AWS_S3_REGION_NAME = 'eu-west-2' # e.g. us-east-2
+AWS_ACCESS_KEY_ID = 'AKIAIQ3ITTBLKAKFZRTQ'
+AWS_SECRET_ACCESS_KEY = 'Q14yiGN8mpjgMR0xi9bYc9Wd/RorhJyDUOM9XABg'
 
 # Tell django-storages the domain to use to refer to static files.
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
@@ -115,3 +115,4 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 # Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
 # you run `collectstatic`).
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
